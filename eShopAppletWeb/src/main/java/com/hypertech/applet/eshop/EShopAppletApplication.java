9782f8cf781af16a1bd6062c5a414ac73914c6b2
yuqzhang <yuqzhang@ebay.com>
2019-12-21 13:17:27 +0800

    Fixed startup issues.

--- eShopAppletWeb/src/main/java/com/hypertech/applet/eshop/EShopAppletApplication.java
+++ eShopAppletWeb/src/main/java/com/hypertech/applet/eshop/EShopAppletApplication.java
@@ -1,9 +1,11 @@
 package com.hypertech.applet.eshop;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hypertech.applet.eshop.mapper")
 public class EShopAppletApplication {
     public static void main(String[] args) {
         SpringApplication.run(EShopAppletApplication.class, args);
--- eShopAppletWeb/src/main/java/com/hypertech/applet/eshop/controller/UserInformationController.java
+++ eShopAppletWeb/src/main/java/com/hypertech/applet/eshop/controller/UserInformationController.java
@@ -2,7 +2,6 @@ package com.hypertech.applet.eshop.controller;
 
 import com.hypertech.applet.eshop.service.IUserInformationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
